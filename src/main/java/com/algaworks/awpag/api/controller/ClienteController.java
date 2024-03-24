d3ed14d462afd131e40b3cecce2dc47112073a75
Romanojp <jp_romana@outlook.com>
2024-03-24 13:21:17 -0300

    usando jakarta persistense

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,6 +1,9 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +13,12 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-     var cliente1 = new Cliente();
-     cliente1.setId(1);
-     cliente1.setNome("João Romana");
-     cliente1.setTelefone("12438461132132");
-     cliente1.setEmail("Joao@Gmail.com");
-
-        var cliente2 = new Cliente();
-        cliente2.setId(2);
-        cliente2.setNome("Thais");
-        cliente2.setTelefone("1251408610");
-        cliente2.setEmail("thais@Gmail.com");
-
-        return Arrays.asList(cliente1, cliente2);
-
+        return manager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
