650c1eab9926ce008ead1c486c26bd593a063413
Romanojp <jp_romana@outlook.com>
2024-03-24 21:12:56 -0300

    Usando Spring Data Jpa

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,24 +1,26 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import com.algaworks.awpag.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
-
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private final ClienteRepository clienteRepository;
+
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
-        return manager.createQuery("from Cliente", Cliente.class)
-                .getResultList();
+       // return clienteRepository.findByNome("joao");
+        return clienteRepository.findByNomeContaining("th");
+
+
     }
 }
